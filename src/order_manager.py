e08ff85720c3525c44fb0278bd81305c044aa1c3
buysetupAI <ruthsmithtgu32@gmail.com>
2024-12-29 00:54:06 +0300

    Create order_manager.py

---
+++ src/order_manager.py
@@ -0,0 +1,7 @@
+class OrderManager:
+    def __init__(self, solana_client, wallet_manager):
+        self.solana_client = solana_client
+        self.wallet_manager = wallet_manager
+
+    def place_order(self, token, amount):
+        pass  # Implementation of token purchase
